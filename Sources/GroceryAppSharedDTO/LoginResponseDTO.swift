17eeec8787840a572034bcac98c12d1bfd4f4fb5
alvincho617 <156638316+alvincho617@users.noreply.github.com>
2024-09-02 12:40:54 +0900

    rename

--- Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
@@ -11,12 +11,12 @@ public struct LoginResponseDTO: Codable {
     public let error: Bool
     public var reason: String? = nil
     public var token: String? = nil
-    public var userid: UUID? = nil
+    public var userId: UUID? = nil
     
     public init(error: Bool, reason: String? = nil, token: String? = nil, userid: UUID? = nil) {
         self.error = error
         self.reason = reason
         self.token = token
-        self.userid = userid
+        self.userId = userid
     }
 }
